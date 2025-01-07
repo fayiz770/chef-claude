07b1e92d86b827c220d0e7795fe6b713d568b361
Waris Fayiz <mohammadwaris456@gmail.com>
2025-01-07 22:10:08 +0330

    initialize React application with Vite, add main components and styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Content from './Content'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  )
+}
+
+export default App
